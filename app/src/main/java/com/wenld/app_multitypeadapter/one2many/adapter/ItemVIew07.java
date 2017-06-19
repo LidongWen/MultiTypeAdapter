112e0a059636f929aa87ada463a5432353eb78d7
wenld <wenld2014@163.om>
2017-06-19 09:52:53 +0800

    example modification

--- app/src/main/java/com/wenld/app_multitypeadapter/one2many/adapter/ItemVIew07.java
+++ app/src/main/java/com/wenld/app_multitypeadapter/one2many/adapter/ItemVIew07.java
@@ -2,9 +2,6 @@ package com.wenld.app_multitypeadapter.one2many.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -25,7 +22,7 @@ public class ItemVIew07 extends MultiItemView<Bean04> {
     @NonNull
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.item_seven;
     }
 
     @Override
